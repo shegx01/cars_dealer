f935c7817f74bb122b591e5ff94d1f3e0ab163f2
O.O Samson <shegx01@gmail.com>
2019-10-31 19:14:14 +0100

    fixing some issues

--- lib/recursion_ch1/intro.ex
+++ lib/recursion_ch1/intro.ex
@@ -1,4 +1,6 @@
 defmodule RecursiveProgramming.RecursionCh1 do
+  import IO,  only: [puts: 1]
+  import  Integer, only: [mod: 2]
   # introduction to basic recursion concept
   # starting the lectures
   # sum of nth term recursively defineded
@@ -17,7 +19,7 @@ defmodule RecursiveProgramming.RecursionCh1 do
   def fib(n) when is_integer(n) and n > 0, do: do_fib(n)
   def fib(arg), do: raise("Expecting integer but found #{inspect(arg)}}")
   defp do_fib(1), do: 1
-  defp do_fib(2), do: 2
+  defp do_fib(2), do: 1
   defp do_fib(n), do: do_fib(n - 1) + do_fib(n - 2)
   #
 
@@ -37,14 +39,33 @@ defmodule RecursiveProgramming.RecursionCh1 do
   # S(n) = 1 if n = 1
   # sn = S(n-1)+n if n > 1
 
-  def sum(n) when is_integer(n) and n > 0, do: do_sum(n, 1)
+  def sum(n) when is_integer(n) and n > 0, do: do_sum(n, 0)
   def sum(_), do: nil
-  defp do_sum(1, acc), do: acc
+  defp do_sum(0, acc), do: acc
   defp do_sum(n, acc), do: do_sum(n - 1, n + acc)
 
+@doc """
+computing the sum of the digits on a non negative integer
+"""
+def add_digits(n)when is_integer(n) and n > 0, do: do_add_digit(n)
+def add_digits(_), do: nil
+defp do_add_digit(n) when n < 10, do: n
+defp do_add_digit(n), do: do_add_digit(div(n, 10)) + Integer.mod(n, 10)
 
-  # computing predecessor
-  # predecessor n = 0 if n = 0
-  #             (n) + 1 if n > 0
+@doc """
+puts digits reverse vertical
+"""
+def puts_digits_reverse_vertical(n) when is_integer(n) and n > 0 do
+print_reverse_vertical(n)
+
+end
+def puts_digits_reverse_vertical(_), do: nil
+defp print_reverse_vertical(n) when n < 10 ,do: puts n
+defp print_reverse_vertical(n) do
+  puts(mod(n, 10))
+   print_reverse_vertical(div(n, 10))
+end
+
+
 
 end
