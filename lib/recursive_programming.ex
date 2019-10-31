f2a2521079b47036027b79d0471d3e3a52cd6633
O.O Samson <shegx01@gmail.com>
2019-10-31 19:16:21 +0100

    fixing some issues

--- lib/recursive_programming.ex
+++ lib/recursive_programming.ex
@@ -1,18 +1,2 @@
 defmodule RecursiveProgramming do
-  @moduledoc """
-  Documentation for RecursiveProgramming.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> RecursiveProgramming.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
